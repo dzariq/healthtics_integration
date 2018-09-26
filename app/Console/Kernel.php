26bd10ef974b26da9829ed35cf5155bd936465d8
Dzariq <dzariq@senangpay.my>
2018-09-26 22:14:05 +0800

    graph test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\Console;
-
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Console\Commands\Integration;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        \App\Console\Commands\Inspire::class,
-    ];
-
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('inspire')
-                 ->hourly();
-    }
-}
-=======
 <?php
 
 namespace App\Console;
@@ -62,4 +29,3 @@ class Kernel extends ConsoleKernel
                  ->hourly();
     }
 }
->>>>>>> 441eda9e6a53f7fdc083b389fe1a366b92e43921
