8fdabe645d4a73a4ad2acbe44be763f2ce7043cc
Dzariq <dzariq@senangpay.my>
2018-09-26 22:18:37 +0800

    graph test

--- app/Console/Commands/Integration.php
+++ app/Console/Commands/Integration.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\Console\Commands;
-
-use Illuminate\Console\Command;
-use Illuminate\Foundation\Inspiring;
-use DB;
-
-class Integration extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'integration';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = '';
-
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $test = DB::table('eklinikal_all_data')->get();
-        print_r($test);die;
-    }
-}
-=======
 <?php
 
 namespace App\Console\Commands;
@@ -70,4 +33,3 @@ class Integration extends Command
         print_r($test);die;
     }
 }
->>>>>>> 85fcc83f04c7ef868762d8c0f31725790f66ce16
