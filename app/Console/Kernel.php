f28d94386a811756ee5f7fbc6a155569bd674bbd
Dzariq <dzariq@senangpay.my>
2018-09-26 22:09:20 +0800

    graph test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Console\Commands\Integration;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
