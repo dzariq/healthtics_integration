0edc422feaea7413c6a7d5c20a3f77980f03a2bf
Dzariq <dzariq@senangpay.my>
2018-10-02 14:47:23 +0800

    serverCtrl

--- app/Console/Commands/Integration.php
+++ app/Console/Commands/Integration.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\Console\Commands;
-
-use Illuminate\Console\Command;
-use Illuminate\Foundation\Inspiring;
-use DB;
-
-class Integration extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'integration';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = '';
-
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $test = DB::table('eklinikal_all_data')->limit(5)->get();
-        print_r($test);die;
-    }
-}
-=======
 <?php
 
 namespace App\Console\Commands;
@@ -78,4 +41,3 @@ class Integration extends Command
     }
 
 }
->>>>>>> 742f544e38a531b80d1e7f91f204e118b0d33c5a
