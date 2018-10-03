9d63e7ae43085fd94229d072a120e38733116807
Dzariq <dzariq@senangpay.my>
2018-10-03 16:30:18 +0800

    mappings

--- app/Models/PatientAddress.php
+++ app/Models/PatientAddress.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PatientAddress extends Model {
+class PatientAddress extends Model
+{
 
     protected $connection = 'mysql';
     protected $table = 'im_patient_addr';
+    protected $primaryKey = 'impaddr_id';
 
-    public function changeConnection($conn) {
+    public function changeConnection($conn)
+    {
         $this->connection = $conn;
     }
 
--- app/Models/PatientData.php
+++ app/Models/PatientData.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PatientData extends Model {
+class PatientData extends Model
+{
 
     protected $connection = 'mysql';
     protected $table = 'im_patient_biodata';
+    protected $primaryKey = 'impb_id';
 
-    public function changeConnection($conn) {
+    public function changeConnection($conn)
+    {
         $this->connection = $conn;
     }
 
